49e671d51e4f242bebddf974376ec83b2e7eed3e
Zhao Xianghu <zhaoxh@ihep.ac.cn>
2015-01-14 18:51:36 +0800

    add quotes for http proxy in default.local

--- WorkloadManagementSystem/private/bootstrap/cvmfs-ihep-context.sh
+++ WorkloadManagementSystem/private/bootstrap/cvmfs-ihep-context.sh
@@ -10,7 +10,7 @@ then
     echo "cvmfs-ihep-context.sh <cvmfs_http_proxy>"
 fi	
 
-sed -i "s+CVMFS_HTTP_PROXY\s*=.*+CVMFS_HTTP_PROXY=$1+" /etc/cvmfs/default.local
+sed -i "s+CVMFS_HTTP_PROXY\s*=.*+CVMFS_HTTP_PROXY=\"$1\"+" /etc/cvmfs/default.local
 
 # reaload configuration to activate new setup:
 /sbin/service autofs restart >> /var/log/cvmfs-boss-script.log 2>&1
